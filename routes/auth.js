2255ed7cad8942ca1e2dfd2c22f3a143d92ba0f9
Saujan Bindukar <sauzanbindukar@gmail.com>
2023-10-14 13:15:14 +0545

    password encrypted

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
 //importng user from userModel
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+const bcryptjs = require('bcryptjs');
 
 const authRouter = express.Router();
 
@@ -8,22 +10,24 @@ authRouter.post('/api/signup', async (req, res) => {
 
     try {
         ///accesing the request body send from the client
-        const { name, email, password } = req.body;
+        const { name, email, password, address } = req.body;
 
         //find existing user if available
         const existingUser = await User.findOne({ email });
 
         //doesnot return bool but return object 
         if (existingUser) {
-            return res.status().json({ message: 'User with this email already exists' });
+            return res.status(400).json({ message: 'User with this email already exists' });
         }
+        //salt is random string 
+        const hashPassword = await bcryptjs.hash(password, 8);
 
         ///creating user with new keyword
         //can use let or var 
         let user = new User({
             email,
             name,
-            password,
+            password: hashPassword,
             address,
         });
 
