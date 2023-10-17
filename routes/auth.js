430d0084ebb4ebba8b611a94619e4758b8379c8a
Saujan Bindukar <sauzanbindukar@gmail.com>
2023-10-17 19:07:11 +0545

    sign up api added , with jwt token

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,8 @@
 const express = require('express');
 //importng user from userModel
 const User = require('../models/user');
-const bcrypt = require('bcryptjs');
 const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const authRouter = express.Router();
 
@@ -40,11 +40,40 @@ authRouter.post('/api/signup', async (req, res) => {
 
 
     //String is object in JS
-
     //get data from client 
     //save to database
-    // return response to client
+    //return response to client
 
+});
+
+
+//signin route
+authRouter.post('/api/signin', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            //user doesnot exist
+            return res.status(400).json({ msg: 'User with this email doesnot exist' });
+        }
+        //now compare the hash password
+        //we had encrpt the password while signup, so now it needs to be decrypt
+        const isMatch = await bcryptjs.compare(password, user.password);
+        if (!isMatch) {
+            //user doesnot exist
+            return res.status(400).json({ msg: 'Password doesnot match' });
+        }
+        //generating the token
+        const token = jwt.sign({ _id: user._id }, "passwordKey");
+
+        return res.status(200).json({ token, ...user._doc });
+
+
+
+    } catch (error) {
+        return res.status(500).json({ error: e.message })
+
+    }
 });
 
 
